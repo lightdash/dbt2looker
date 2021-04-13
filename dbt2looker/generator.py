f041ec1842efc2d44b2b341b54719cf898b2f819
Katie Hindson <hindsonk@gmail.com>
2021-04-13 16:00:51 +0200

    adds iteration through list of columns

--- dbt2looker/generator.py
+++ dbt2looker/generator.py
@@ -1,7 +1,21 @@
 from . import models
 
 def lookml_view_from_dbt_model(model: models.DBTModel):
+    contents = ''
+    for column in model.columns:
+        column_vals = column_vals + """
+            dimension: {} {
+              description: {}
+              type: string # need to change type to be pulled from BQ schema
+              sql: ${TABLE}.{};;
+            }
+        """.format(model.name, column.description, model.name)
+
     return models.LookViewFile(
-        filename='test.view',
-        contents='test test test',
+        filename='{}.view'.format(model.name),
+        contents= """
+        view: {} {
+          sql_table_name: `{}.{}` ;;
+
+        """.format(model.name, model.schema, model.name) + column_vals + "\n}",
     )
