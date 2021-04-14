3d6252fc187bc639a7cb072ab2ed0eabe277554b
Katie Hindson <hindsonk@gmail.com>
2021-04-14 16:20:40 +0200

    adds dtype mapping

--- dbt2looker/generator.py
+++ dbt2looker/generator.py
@@ -4,6 +4,33 @@ from . import models
 
 
 def lookml_view_from_dbt_model(model: models.DbtModel):
+    map_bq_to_looker_dtypes = {
+        "INTEGER":   "number",
+    	"FLOAT":     "number",
+    	"NUMERIC":   "number",
+    	"BOOLEAN":   "yesno",
+    	"STRING":    "string",
+    	"TIMESTAMP": "time",
+    	"DATETIME":  "time",
+    	"DATE":      "time",
+    	"TIME":      "time",
+    	"BOOL":      "yesno",
+    	"ARRAY":     "string",
+    	"GEOGRAPHY": "string",
+    }
+
+    timeframe = """
+    timeframes: [
+      raw,
+      time,
+      date,
+      week,
+      month,
+      quarter,
+      year
+    ]
+    """
+
     lookml = {
         'view': {
             'name': model.name,
