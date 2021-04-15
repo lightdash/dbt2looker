51ccca6169a8a56ebb03c72c13e2e5ccaa669da5
oliverlaslett <olaslett@gmail.com>
2021-04-15 14:38:40 +0100

    add snowflake + redshift support. add warnings

--- dbt2looker/generator.py
+++ dbt2looker/generator.py
@@ -1,23 +1,95 @@
+import logging
 from typing import List
 
 import lkml
 
 from . import models
 
-map_to_looker_dtypes = {
+LOOKER_DTYPE_MAP = {
     'bigquery': {
-        "INTEGER":   "number",
-        "FLOAT":     "number",
-        "NUMERIC":   "number",
-        "BOOLEAN":   "yesno",
-        "STRING":    "string",
-        "TIMESTAMP": "timestamp",
-        "DATETIME":  "datetime",
-        "DATE":      "date",
-        "TIME":      "string",    # Can time-only be handled better in looker?
-        "BOOL":      "yesno",
-        "ARRAY":     "string",
-        "GEOGRAPHY": "string",
+        'INTEGER':   'number',
+        'FLOAT':     'number',
+        'NUMERIC':   'number',
+        'BOOLEAN':   'yesno',
+        'STRING':    'string',
+        'TIMESTAMP': 'timestamp',
+        'DATETIME':  'datetime',
+        'DATE':      'date',
+        'TIME':      'string',    # Can time-only be handled better in looker?
+        'BOOL':      'yesno',
+        'ARRAY':     'string',
+        'GEOGRAPHY': 'string',
+    },
+    'snowflake': {
+        'NUMBER': 'number',
+        'DECIMAL': 'number',
+        'NUMERIC': 'number',
+        'INT': 'number',
+        'INTEGER': 'number',
+        'BIGINT': 'number',
+        'SMALLINT': 'number',
+        'FLOAT': 'number',
+        'FLOAT4': 'number',
+        'FLOAT8': 'number',
+        'DOUBLE': 'number',
+        'DOUBLE PRECISION': 'number',
+        'REAL': 'number',
+        'VARCHAR': 'string',
+        'CHAR': 'string',
+        'CHARACTER': 'string',
+        'STRING': 'string',
+        'TEXT': 'string',
+        'BINARY': 'string',
+        'VARBINARY': 'string',
+        'BOOLEAN': 'yesno',
+        'DATE': 'date',
+        'DATETIME': 'datetime',
+        'TIME': 'string',        # can we support time?
+        'TIMESTAMP': 'timestamp',
+        'TIMESTAMP_NTZ': 'timestamp',
+        # TIMESTAMP_LTZ not supported (see https://docs.looker.com/reference/field-params/dimension_group)
+        # TIMESTAMP_TZ not supported (see https://docs.looker.com/reference/field-params/dimension_group)
+        'VARIANT': 'string',
+        'OBJECT': 'string',
+        'ARRAY': 'string',
+        'GEOGRAPHY': 'string',
+    },
+    'redshift': {
+        'SMALLINT': 'number',
+        'INT2': 'number',
+        'INTEGER': 'number',
+        'INT': 'number',
+        'INT4': 'number',
+        'BIGINT': 'number',
+        'INT8': 'number',
+        'DECIMAL': 'number',
+        'NUMERIC': 'number',
+        'REAL': 'number',
+        'FLOAT4': 'number',
+        'DOUBLE PRECISION': 'number',
+        'FLOAT8': 'number',
+        'FLOAT': 'number',
+        'BOOLEAN': 'yesno',
+        'BOOL': 'yesno',
+        'CHAR': 'string',
+        'CHARACTER': 'string',
+        'NCHAR': 'string',
+        'BPCHAR': 'string',
+        'VARCHAR': 'string',
+        'CHARACTER VARYING': 'string',
+        'NVARCHAR': 'string',
+        'TEXT': 'string',
+        'DATE': 'date',
+        'TIMESTAMP': 'timestamp',
+        'TIMESTAMP WITHOUT TIME ZONE': 'timestamp',
+        # TIMESTAMPTZ not supported
+        # TIMESTAMP WITH TIME ZONE not supported
+        'GEOMETRY': 'string',
+        # HLLSKETCH not supported
+        'TIME': 'string',
+        'TIME WITHOUT TIME ZONE': 'string',
+        # TIMETZ not supported
+        # TIME WITH TIME ZONE not supported
     }
 }
 
@@ -36,13 +108,20 @@ looker_timeframes = [
 ]
 
 
+def map_adapter_type_to_looker(adapter_type: models.SupportedDbtAdapters, column_type: str):
+    looker_type = LOOKER_DTYPE_MAP[adapter_type].get(column_type)
+    if looker_type is None:
+        logging.warn(f'Column type {column_type} not supported for conversion from {adapter_type} to looker. No dimension will be created.')
+    return looker_type
+
+
 def lookml_date_time_dimension_group(column: models.DbtModelColumn, adapter_type: models.SupportedDbtAdapters):
     return {
         'name': column.name,
         'type': 'time',
         'sql': f'${{TABLE}}.{column.name}',
         'description': column.description,
-        'datatype': map_to_looker_dtypes[adapter_type][column.data_type],
+        'datatype': map_adapter_type_to_looker(adapter_type, column.data_type),
         'timeframes': ['raw', 'time', 'hour', 'date', 'week', 'month', 'quarter', 'year']
     }
 
@@ -53,7 +132,7 @@ def lookml_date_dimension_group(column: models.DbtModelColumn, adapter_type: mod
         'type': 'time',
         'sql': f'${{TABLE}}.{column.name}',
         'description': column.description,
-        'datatype': map_to_looker_dtypes[adapter_type][column.data_type],
+        'datatype': map_adapter_type_to_looker(adapter_type, column.data_type),
         'timeframes': ['raw', 'date', 'week', 'month', 'quarter', 'year']
     }
 
@@ -62,12 +141,12 @@ def lookml_dimension_groups_from_model(model: models.DbtModel, adapter_type: mod
     date_times = [
         lookml_date_time_dimension_group(column, adapter_type)
         for column in model.columns.values()
-        if map_to_looker_dtypes[adapter_type][column.data_type] in looker_date_time_types
+        if map_adapter_type_to_looker(adapter_type, column.data_type) in looker_date_time_types
     ]
     dates = [
         lookml_date_dimension_group(column, adapter_type)
         for column in model.columns.values()
-        if map_to_looker_dtypes[adapter_type][column.data_type] in looker_date_types
+        if map_adapter_type_to_looker(adapter_type, column.data_type) in looker_date_types
     ]
     return date_times + dates
 
@@ -76,12 +155,12 @@ def lookml_dimensions_from_model(model: models.DbtModel, adapter_type: models.Su
     return [
         {
             'name': column.name,
-            'type': map_to_looker_dtypes[adapter_type][column.data_type],
+            'type': map_adapter_type_to_looker(adapter_type, column.data_type),
             'sql': f'${{TABLE}}.{column.name}',
             'description': column.description
         }
         for column in model.columns.values()
-        if map_to_looker_dtypes[adapter_type][column.data_type] in looker_scalar_types
+        if map_adapter_type_to_looker(adapter_type, column.data_type) in looker_scalar_types
     ]
 
 
