44a41cfdb2a27420e8b4f4762ce6346880fcdd33
dkruh1 <101973979+dkruh1@users.noreply.github.com>
2023-11-16 10:49:05 +0200

    wip

--- tests/yoda_dbt2looker/test_cli.py
+++ tests/yoda_dbt2looker/test_cli.py
@@ -18,15 +18,16 @@ def test__run_convert():
 
 def remove_folder_contents(folder_path):
     # Remove the contents of the folder, but keep the folder itself
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f"Failed to delete {file_path}. Reason: {e}")
+    if os.path.exists(folder_path):
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
 
 
 def assert_folders_equal(folder1, folder2):
